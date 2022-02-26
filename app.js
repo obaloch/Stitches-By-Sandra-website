dc9d856ad26007efee6d4afe4314251b42943746
obaloch1997 <100452153+obaloch1997@users.noreply.github.com>
2022-02-25 19:13:31 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const getElement = (selector) => {
+    const element = document.querySelector(selector);
+    if(element) return element
+    throw Error(`Please double check your class names, there is no ${selector} class`
+    )
+} 
+
+const links = getElement('.nav-links')
+const botton = getElement('.nav-btn')
+
+botton.addEventListener('click', () => {
+    links.classList.toggle('show-links')
+})
